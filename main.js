bb2d67c63b783f122f688e53b46025226e061b2c
arturgomesc <arturgomesc@hotmail.com>
2023-04-03 17:52:11 -0300

    adicionando js

---
+++ main.js
@@ -0,0 +1,11 @@
+const listaDeTeclas = document.querySelectorAll('.teclado_numbers');
+const inputTel = document.querySelector('input[type=tel]');
+
+for(let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+
+    tecla.onclick = () => {
+        inputTel.value += tecla.value;
+    }
+
+}
